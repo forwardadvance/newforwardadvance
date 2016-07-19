bf8c7f2ccfdd428cdcaf346297b25c43cc08bd98
Nicholas Johnson <nicholas@forwardadvance.com>
2016-07-19 20:22:27 +0100

    Facebook and Github auth

--- config/passport.js
+++ config/passport.js
@@ -110,114 +110,114 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, passw
 /**
  * Sign in with Facebook.
  */
-// passport.use(new FacebookStrategy({
-//   clientID: process.env.FACEBOOK_ID,
-//   clientSecret: process.env.FACEBOOK_SECRET,
-//   callbackURL: '/auth/facebook/callback',
-//   profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
-//   passReqToCallback: true
-// }, function(req, accessToken, refreshToken, profile, done) {
-//   if (req.user) {
-//     User.findOne({ facebook: profile.id }, function(err, existingUser) {
-//       if (existingUser) {
-//         req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-//         done(err);
-//       } else {
-//         User.findById(req.user.id, function(err, user) {
-//           user.facebook = profile.id;
-//           user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-//           user.profile.name = user.profile.name || profile.name.givenName + ' ' + profile.name.familyName;
-//           user.profile.gender = user.profile.gender || profile._json.gender;
-//           user.profile.picture = user.profile.picture || 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-//           user.save(function(err) {
-//             req.flash('info', { msg: 'Facebook account has been linked.' });
-//             done(err, user);
-//           });
-//         });
-//       }
-//     });
-//   } else {
-//     User.findOne({ facebook: profile.id }, function(err, existingUser) {
-//       if (existingUser) {
-//         return done(null, existingUser);
-//       }
-//       User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
-//         if (existingEmailUser) {
-//           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
-//           done(err);
-//         } else {
-//           var user = new User();
-//           user.email = profile._json.email;
-//           user.facebook = profile.id;
-//           user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-//           user.profile.name = profile.name.givenName + ' ' + profile.name.familyName;
-//           user.profile.gender = profile._json.gender;
-//           user.profile.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-//           user.profile.location = (profile._json.location) ? profile._json.location.name : '';
-//           user.save(function(err) {
-//             done(err, user);
-//           });
-//         }
-//       });
-//     });
-//   }
-// }));
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL: '/auth/facebook/callback',
+  profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
+  passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, done) {
+  if (req.user) {
+    User.findOne({ facebook: profile.id }, function(err, existingUser) {
+      if (existingUser) {
+        req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      } else {
+        User.findById(req.user.id, function(err, user) {
+          user.facebook = profile.id;
+          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
+          user.profile.name = user.profile.name || profile.name.givenName + ' ' + profile.name.familyName;
+          user.profile.gender = user.profile.gender || profile._json.gender;
+          user.profile.picture = user.profile.picture || 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
+          user.save(function(err) {
+            req.flash('info', { msg: 'Facebook account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ facebook: profile.id }, function(err, existingUser) {
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
+        if (existingEmailUser) {
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
+          done(err);
+        } else {
+          var user = new User();
+          user.email = profile._json.email;
+          user.facebook = profile.id;
+          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
+          user.profile.name = profile.name.givenName + ' ' + profile.name.familyName;
+          user.profile.gender = profile._json.gender;
+          user.profile.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
+          user.profile.location = (profile._json.location) ? profile._json.location.name : '';
+          user.save(function(err) {
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
 
 /**
  * Sign in with GitHub.
  */
-// passport.use(new GitHubStrategy({
-//   clientID: process.env.GITHUB_ID,
-//   clientSecret: process.env.GITHUB_SECRET,
-//   callbackURL: '/auth/github/callback',
-//   passReqToCallback: true
-// }, function(req, accessToken, refreshToken, profile, done) {
-//   if (req.user) {
-//     User.findOne({ github: profile.id }, function(err, existingUser) {
-//       if (existingUser) {
-//         req.flash('errors', { msg: 'There is already a GitHub account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-//         done(err);
-//       } else {
-//         User.findById(req.user.id, function(err, user) {
-//           user.github = profile.id;
-//           user.tokens.push({ kind: 'github', accessToken: accessToken });
-//           user.profile.name = user.profile.name || profile.displayName;
-//           user.profile.picture = user.profile.picture || profile._json.avatar_url;
-//           user.profile.location = user.profile.location || profile._json.location;
-//           user.profile.website = user.profile.website || profile._json.blog;
-//           user.save(function(err) {
-//             req.flash('info', { msg: 'GitHub account has been linked.' });
-//             done(err, user);
-//           });
-//         });
-//       }
-//     });
-//   } else {
-//     User.findOne({ github: profile.id }, function(err, existingUser) {
-//       if (existingUser) {
-//         return done(null, existingUser);
-//       }
-//       User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
-//         if (existingEmailUser) {
-//           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
-//           done(err);
-//         } else {
-//           var user = new User();
-//           user.email = profile._json.email;
-//           user.github = profile.id;
-//           user.tokens.push({ kind: 'github', accessToken: accessToken });
-//           user.profile.name = profile.displayName;
-//           user.profile.picture = profile._json.avatar_url;
-//           user.profile.location = profile._json.location;
-//           user.profile.website = profile._json.blog;
-//           user.save(function(err) {
-//             done(err, user);
-//           });
-//         }
-//       });
-//     });
-//   }
-// }));
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_ID,
+  clientSecret: process.env.GITHUB_SECRET,
+  callbackURL: '/auth/github/callback',
+  passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, done) {
+  if (req.user) {
+    User.findOne({ github: profile.id }, function(err, existingUser) {
+      if (existingUser) {
+        req.flash('errors', { msg: 'There is already a GitHub account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      } else {
+        User.findById(req.user.id, function(err, user) {
+          user.github = profile.id;
+          user.tokens.push({ kind: 'github', accessToken: accessToken });
+          user.profile.name = user.profile.name || profile.displayName;
+          user.profile.picture = user.profile.picture || profile._json.avatar_url;
+          user.profile.location = user.profile.location || profile._json.location;
+          user.profile.website = user.profile.website || profile._json.blog;
+          user.save(function(err) {
+            req.flash('info', { msg: 'GitHub account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ github: profile.id }, function(err, existingUser) {
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
+        if (existingEmailUser) {
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
+          done(err);
+        } else {
+          var user = new User();
+          user.email = profile._json.email;
+          user.github = profile.id;
+          user.tokens.push({ kind: 'github', accessToken: accessToken });
+          user.profile.name = profile.displayName;
+          user.profile.picture = profile._json.avatar_url;
+          user.profile.location = profile._json.location;
+          user.profile.website = profile._json.blog;
+          user.save(function(err) {
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
 
 // Sign in with Twitter.
 
