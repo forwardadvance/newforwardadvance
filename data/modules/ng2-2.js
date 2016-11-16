bf104c2574a98fe6051abd77eca3029909847c56
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-16 10:08:02 +0000

    Exercise moved

--- data/modules/ng2-2.js
+++ data/modules/ng2-2.js
@@ -15,6 +15,14 @@ module.exports = {
       path: 'ng2/nested-components'
     },
     {
+      title: 'Built In Template Directives, ngIf & ngFor',
+      slug: 'template-directives',
+      description: '',
+      github: 'angular2_ngif_ngrepeat_demo',
+      learnings: [],
+      path: 'ng2/template-directives'
+    },
+    {
       title: 'Making Angular 2 Components Receive Values (inputs)',
       slug: 'component-inputs',
       description: '',
@@ -31,14 +39,6 @@ module.exports = {
       homepage: true,
       learnings: [],
       path: 'ng2/event-emitters'
-    },
-    {
-      title: 'Built In Template Directives, ngIf, ngRepeat, ngSwitch',
-      slug: 'template-directives',
-      description: '',
-      github: 'angular2_template_directives_demo',
-      learnings: [],
-      path: 'ng2/template-directives'
     },
     {
       title: 'Two-Way-Binding Forms in Angular 2',
