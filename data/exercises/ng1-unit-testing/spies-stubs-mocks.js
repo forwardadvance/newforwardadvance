fd589a9ff7ae2e2cdb84134ea9a2d78909605e08
Nicholas Johnson <nicholas@forwardadvance.com>
2017-01-11 15:28:57 +0000

    github repos

--- data/exercises/ng1-unit-testing/spies-stubs-mocks.js
+++ data/exercises/ng1-unit-testing/spies-stubs-mocks.js
@@ -2,6 +2,7 @@ module.exports = {
   slug: 'spies-stubs-mocks',
   title: 'What are Stubs, Spies and Mocks',
   description: '',
+  github: 'angular_unit_testing_spies_stubs_mocks',
   learnings: [
   ]
 }
--- data/exercises/ng1-unit-testing/webpack.js
+++ data/exercises/ng1-unit-testing/webpack.js
@@ -2,6 +2,7 @@ module.exports = {
   slug: 'webpack',
   title: 'Running Jasmine in the browser with Webpack',
   description: '',
+  github: 'angular_unit_testing_webpack',
   learnings: [
   ]
 }
