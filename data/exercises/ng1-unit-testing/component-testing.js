8f75c1139ecceae7813f75e0b9221decfab80856
Nicholas Johnson <nicholas@forwardadvance.com>
2017-01-12 09:42:07 +0000

    Components

--- data/exercises/ng1-unit-testing/component-testing.js
+++ data/exercises/ng1-unit-testing/component-testing.js
@@ -2,6 +2,7 @@ module.exports = {
   slug: 'component-testing',
   title: 'Unit Testing Angular Directives and Components',
   description: '',
+  github: 'angular_unit_testing_components',
   learnings: [
   ]
 }
